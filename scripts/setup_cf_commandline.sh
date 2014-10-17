548f9b2705716e3bd4edfe7067a35bd9112b3b61
Rahul Jain <rahulkj@gmail.com>
2014-10-17 15:48:23 -0500

    Moved the error trap to the right location

--- scripts/setup_cf_commandline.sh
+++ scripts/setup_cf_commandline.sh
@@ -6,15 +6,17 @@ export CLOUD_CONTROLLER_URL=https://api.10.244.0.34.xip.io/
 export ORG_NAME=local
 export SPACE_NAME=development
 
-set -e
+export EXECUTION_DIR=$PWD
+export LOG_FILE=$EXECUTION_DIR/setup.log
+
 echo "###### Setup cloudfoundry cli ######"
 GO_CF_VERSION=`which cf`
 if [ -z "$GO_CF_VERSION" ]; then
   brew tap pivotal/tap >> $LOG_FILE 2>&1
   brew install cloudfoundry-cli >> $LOG_FILE 2>&1
-#	echo $PASSWORD | sudo -S ln -s /usr/local/bin/cf /usr/local/bin/gcf
 fi
 
+set -e
 echo "###### Setting up cf (Create org, spaces) ######"
 cf api --skip-ssl-validation $CLOUD_CONTROLLER_URL >> $LOG_FILE 2>&1
 cf auth $CF_USER $CF_PASSWORD >> $LOG_FILE 2>&1
